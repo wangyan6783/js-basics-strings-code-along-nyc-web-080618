84277fc7926e4bf4544eea3b5baee7ea7e74d035
wangyan6783 <wangyan6783@gmail.com>
2018-09-15 05:46:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Yan"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser[0]}!`
